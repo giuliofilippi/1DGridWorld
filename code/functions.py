6b3d53afba22cdad0736d89e7f603272b0ae35cb
giuliofilippi <giuliofilippi@me.com>
2023-12-13 22:20:35 +0000

    req

---
+++ code/functions.py
@@ -0,0 +1,29 @@
+# base imports
+import numpy as np
+
+# get local data matrix
+def local_neighborhood(vector, radius):
+    """
+    Extracts the local neighborhood of each element in a vector with periodic boundary conditions.
+
+    Parameters:
+    - vector (numpy.ndarray): The input vector.
+    - radius (int): The radius of the local neighborhood.
+
+    Returns:
+    - numpy.ndarray: A 2D array where each row corresponds to a neighborhood.
+    """
+    length = len(vector)
+    neighborhoods = []
+
+    for i in range(length):
+        # Create an index array for the local neighborhood
+        neighbors_indices = (np.arange(-radius, radius + 1) + i) % length
+
+        # Extract values from the vector using the index array
+        neighborhood_values = vector[neighbors_indices]
+
+        # append to list of neighbourhoods
+        neighborhoods.append(neighborhood_values)
+
+    return np.array(neighborhoods)
